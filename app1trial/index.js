b93cc1bb86ae907b26f963768b673b86ec7c3f72
jadrum <justindrum12@gmail.com>
2016-06-13 11:50:07 -0400

    Still attempting to figure out proper setup

--- app1trial/index.js
+++ app1trial/index.js
@@ -3,10 +3,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
+import updaterApp from './reducers'
+import App from './components/App.js'
 
-let store = createStore(todoApp)
+
+
+let store = createStore(updaterApp)
 
 render(
   <Provider store={store}>
---
+++ app1trial/reducers/updater.js
@@ -0,0 +1,18 @@
+const updater = (state, action) => {
+ if(typeof state === 'undefined'){
+    return "working"
+  } 
+
+  switch(action.type) {
+    case 'WORKING':
+      return "working."
+    case 'ATMEETING':
+      return "at a meeting."
+    case 'ONBREAK':
+      return "on my break."
+    default:
+      return state
+  }
+}
+
+export default updater 
