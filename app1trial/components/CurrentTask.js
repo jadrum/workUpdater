f85c1b8dfb6c73c4ff9af569a08fc160ee7cadf1
jadrum <justindrum12@gmail.com>
2016-06-15 16:27:06 -0400

    The reducers now successfully edit the app. Next I will clean up naming conventions and get everything finished to start working on the tracker.

--- app1trial/components/CurrentTask.js
+++ app1trial/components/CurrentTask.js
@@ -1,31 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { task } from '../actions'
+import ButtonBody from '../containers/ButtonBody'
 
-let CurrentTask = ({ dispatch }) => (
+let CurrentTask = (store) => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-xs-12 top">
         <div className="panel panel-info">
           <div className="panel-heading txtCol">
-            <h4>I am put the current task here </h4>
+            <h4>I am {store.updater}</h4>  
           </div>
-          <div className="panel-body">
-            <ul className="list-unstyled list-inline text-center">
-              <li> 
-                <button className="btn btn-primary" 
-                onClick={ () => dispatch(task('working')) }>Working</button>
-              </li>
-              <li>
-                <button className="btn btn-primary"
-                onClick={ () => dispatch(task('atmeeting')) }>At a meeting</button> 
-              </li>
-              <li>
-                <button className="btn btn-primary"
-                onClick={ () => dispatch(task('onbreak')) }>On my break</button>
-              </li>
-            </ul>
-          </div>
+        <ButtonBody />
         </div>
       </div>
     </div> 
--- app1trial/components/Status.js
+++ app1trial/components/Status.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Status = () => (
+const Status = (state) => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-xs-12">
         <div className="panel panel-info" >
           <div className="panel-heading"><h4>Most recent update</h4></div>
           <div className="panel-body">
-            Put the form info here  
+            {state.form}    
           </div>  
         </div>
       </div>
