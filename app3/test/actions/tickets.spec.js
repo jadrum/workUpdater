827e8c227f8daf909b72b624799435c0c1adfdac
jadrum <justindrum12@gmail.com>
2016-07-13 14:51:47 -0400

    Added mocha tests for the actions and reducers

---
+++ app3/test/actions/tickets.spec.js
@@ -0,0 +1,23 @@
+import expect from 'expect'
+import * as actions from '../../actions'
+
+describe('ticket panel action tests', () => {
+  it('payload should be assigned properly', () => {
+    expect(actions.ticketUpdate({
+      author: "Justin",
+      ticketNum: "TS-135",
+      date: "1/1/1111",
+      time: "1:11",
+      currentStatus: "Closed",
+      details: "I have been testing all day"
+        })).toEqual({
+          type: "TICKET_UPDATE",
+          author: "Justin",
+          ticketNum: "TS-135",
+          date: "1/1/1111",
+          time: "1:11",
+          currentStatus: "Closed",
+          details: "I have been testing all day"
+        })
+  })
+})
---
+++ app3/test/reducers/tickets.spec.js
@@ -0,0 +1,75 @@
+import expect from 'expect'
+import ticketPanel from '../../reducers/ticketPanel'
+
+describe('ticket panel reducer tests', () => {
+  it('the reducer should obtain correct initial state', () => {
+    expect(
+      ticketPanel(undefined, {})
+    ).toEqual([{
+      author: "fake author",
+      ticketNumber: "1234",
+      timeSpent: "12h",
+      currentStatus: "Working"
+    }])
+  })
+
+  it('the reducer should add ticket updates properly', () => {
+    expect(
+      ticketPanel([], {
+        type: 'TICKET_UPDATE',
+        author: "Justin",
+        ticketNum: "TS-135",
+        date: "1/1/1111",
+        time: "1:11",
+        currentStatus: "In Progress",
+        details: "I have been working all day"
+      })
+    ).toEqual([
+      {
+        author: "Justin",
+        ticketNum: "TS-135",
+        date: "1/1/1111",
+        time: "1:11",
+        currentStatus: "In Progress",
+        details: "I have been working all day"
+      }
+    ])
+
+    expect(
+      ticketPanel([
+        {
+          author: "Justin",
+          ticketNum: "TS-135",
+          date: "1/1/1111",
+          time: "1:11",
+          currentStatus: "In Progress",
+          details: "I have been working all day"
+        }
+      ], {
+        type: 'TICKET_UPDATE',
+        author: "Dylan",
+        ticketNum: "TS-136",
+        date: "1/1/1112/",
+        time: "1:12",
+        currentStatus: "Closed",
+        details: "I have been working all day too"
+      })
+    ).toEqual([
+      {
+        author: "Justin",
+        ticketNum: "TS-135",
+        date: "1/1/1111",
+        time: "1:11",
+        currentStatus: "In Progress",
+        details: "I have been working all day"
+      }, {
+        author: "Dylan",
+        ticketNum: "TS-136",
+        date: "1/1/1112/",
+        time: "1:12",
+        currentStatus: "Closed",
+        details: "I have been working all day too"
+      }
+    ])
+  })      
+})
