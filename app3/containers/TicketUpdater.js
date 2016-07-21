874baed4ab80fda866135f26fbd83d374ff2391e
jadrum <justindrum12@gmail.com>
2016-07-21 14:25:21 -0400

    Finished the tests for the form

--- app3/containers/TicketUpdater.js
+++ app3/containers/TicketUpdater.js
@@ -6,8 +6,57 @@ import { bindActionCreators } from 'redux'
 export class TicketUpdater extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      author: '',
+      ticketNum: '',
+      date: '',
+      time: '',
+      currentStatus: 'In Progress',
+      details: ''
+    }
+    this.onAuthorChange = this.onAuthorChange.bind(this);
+    this.onTicketNumChange = this.onTicketNumChange.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
+    this.onTimeChange = this.onTimeChange.bind(this);
+    this.onCurrentStatusChange = this.onCurrentStatusChange.bind(this);
+    this.onDetailsChange = this.onDetailsChange.bind(this);
+  }
+  
+  onAuthorChange(e) {
+    this.setState({
+      author: e.target.value
+    })
   }
 
+  onTicketNumChange(e) {
+    this.setState({
+      ticketNum: e.target.value
+    })
+  }
+  
+  onDateChange(e) {
+    this.setState({
+      date: e.target.value
+    })
+  }
+
+  onTimeChange(e) {
+    this.setState({
+      time: e.target.value
+    })
+  }
+
+  onCurrentStatusChange(e) {
+    this.setState({
+      currentStatus: e.target.value
+    })
+  }
+
+  onDetailsChange(e) {
+    this.setState({
+      details: e.target.value
+    })
+  }
 
   render() {
     let author, ticketNum, date, time, currentStatus, details;
@@ -18,28 +67,28 @@ export class TicketUpdater extends Component {
             <form className="form-horizontal top form-format"
               onSubmit={e => {
                 e.preventDefault()
-                if (!author.value.trim()
-                      || !ticketNum.value.trim()
-                      || !date.value.trim()
-                      || !time.value.trim()
-                      || !currentStatus.value.trim()
-                      || !details.value.trim()) {
+                if (!this.refs.author.value.trim()
+                      || !this.refs.ticketNum.value.trim()
+                      || !this.refs.date.value.trim()
+                      || !this.refs.time.value.trim()
+                      || !this.refs.currentStatus.value.trim()
+                      || !this.refs.details.value.trim()) {
                       return
                 }
                 this.props.ticketUpdate({
-                  author: author.value,
-                  ticketNum: ticketNum.value,
-                  date: date.value,
-                  time: time.value,
-                  currentStatus: currentStatus.value,
-                  details: details.value
+                  author: this.refs.author.value,
+                  ticketNum: this.refs.ticketNum.value,
+                  date: this.refs.date.value,
+                  time: this.refs.time.value,
+                  currentStatus: this.refs.currentStatus.value,
+                  details: this.refs.details.value
                     })
-                author.value = ''
-                ticketNum.value = ''
-                date.value = ''
-                time.value = ''
-                currentStatus.value = ''
-                details.value = ''
+                this.refs.author.value = ''
+                this.refs.ticketNum.value = ''
+                this.refs.date.value = ''
+                this.refs.time.value = ''
+                this.refs.currentStatus.value = ''
+                this.refs.details.value = ''
 
               }}>
               
@@ -48,14 +97,18 @@ export class TicketUpdater extends Component {
                 <div className="col-xs-7">
                   <input type="Author" className="form-control" 
                     placeholder="Who worked on the ticket?"
-                    ref={node => { author = node }}>
+                    ref="author"
+                    onChange={this.onAuthorChange}
+                    value={this.state.author}>
                       </input>
                 </div>
                 <label className="col-xs-2 control-label txtCol">Ticket Number</label>
                 <div className="col-xs-2">
                   <input type="ticketNum" className="form-control"
                     placeholder="Ticket number?"
-                    ref={node => {ticketNum = node }}>
+                    ref="ticketNum"
+                    onChange={this.onTicketNumChange}
+                    value={this.state.ticketNum}>
                       </input>
                 </div>
               </div>
@@ -64,21 +117,27 @@ export class TicketUpdater extends Component {
                 <label className="col-xs-1 control-label txtCol">Date</label>
                 <div className="col-xs-3">
                   <input type="date" className="form-control"
-                    ref={node => {date = node }}>
+                    ref="date"
+                    onChange={this.onDateChange}
+                    value={this.state.date}>
                       </input>
                 </div>
 
                 <label className="col-xs-2 control-label txtCol">Time Spent</label>
                 <div className="col-xs-2">
                   <input type="time" className="form-control"
-                    ref={node => {time = node }}>
+                    ref="time"
+                    onChange={this.onTimeChange}
+                    value={this.state.time}>
                       </input>
                 </div>
 
                 <label className="col-xs-2 control-label txtCol">Status</label>
                 <div className="col-xs-2">
                   <select className="form-control"
-                    ref={node => {currentStatus = node }}>
+                    ref="currentStatus"
+                    onChange={this.onCurrentStatusChange}
+                    value={this.state.currentStatus}>
                       <option value="In Progress">In Progress</option>
                       <option value="Resolved">Resolved</option>
                       <option value="Reopened">Reopened</option>
@@ -92,7 +151,9 @@ export class TicketUpdater extends Component {
                 <div className="col-xs-11">
                   <textarea className="form-control" placeholder="What have you been working on?
                     What do you plan to work on next?"
-                    rows="5" ref={node => { details = node }}>
+                    rows="5" ref="details"
+                    onChange={this.onDetailsChange}
+                    value={this.state.details}>
                       </textarea>
                 </div>
               </div>
@@ -102,7 +163,6 @@ export class TicketUpdater extends Component {
                   <button type="Submit" className="btn">Submit</button>
                 </div>
               </div>
-
             </form>
           </div>
         </div>
--- app3/test/components/updater.spec.js
+++ app3/test/components/updater.spec.js
@@ -16,16 +16,45 @@ function setup() {
   const props = {
     ticketUpdate: sinon.spy() 
   }
-
+  const state = {
+    author: '',
+    ticketNum: '',
+    date: '',
+    time: '',
+    currentStatus: 'In Progress',
+    details: ''
+  }
   const wrapper = shallow(<TicketUpdater {...props} />);
-  const form = wrapper.find('form')
-  const button = form.find('button')
+  const form = wrapper.find('form');
 
   return {
     props,
     wrapper,
     form,
-    button
+    state
+  }
+}
+
+function mountSetup() {
+  const props = {
+    ticketUpdate: sinon.spy()
+  }
+  const state = {
+    author: '',
+    ticketNum: '',
+    date: '',
+    time: '',
+    currentStatus: 'In Progress',
+    details: ''
+  }
+  const wrapper = mount(<TicketUpdater {...props} />);
+  const form = wrapper.find('form');
+
+  return {
+    props,
+    wrapper,
+    form,
+    state
   }
 }
 
@@ -37,59 +66,88 @@ describe('Testing the ticket updater component', function() {
     });
 
     it('should have an Author input with correct props', function() {
-      const { wrapper } = setup()
-      expect(wrapper.contains(
-          <input type="Author" className="form-control"
-            placeholder="Who worked on the ticket?"
-            ref={node => { author = node }}>
-              </input>)).to.equal(true);
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      const author = wrapper.ref('author').get(0)
+      expect(author.type).to.equal("author");
+      expect(author.placeholder).to.equal("Who worked on the ticket?");
     });
 
     it('should have a Ticket Number input with correct props', function() {
-      const { wrapper } = setup() 
-      expect(wrapper.contains(
-          <input type="ticketNum" className="form-control"
-            placeholder="Ticket number?"
-            ref={node => {ticketNum = node }}>
-              </input>)).to.equal(true);
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      const ticketNum = wrapper.ref('ticketNum').get(0)
+      expect(ticketNum.type).to.equal("ticketnum");
+      expect(ticketNum.placeholder).to.equal("Ticket number?");
     });
 
     it('should have a Date input with correct props', function() {
-      const { wrapper } = setup()
-      expect(wrapper.contains(
-          <input type="date" className="form-control"
-            ref={node => {date = node }}>
-              </input>)).to.equal(true);
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      const date = wrapper.ref('date').get(0)
+      expect(date.type).to.equal("date");
+      
     });
 
     it('should have a Time input with correct props', function() {
-      const { wrapper } = setup()
-      expect(wrapper.contains(
-          <input type="time" className="form-control"
-            ref={node => {time = node }}>
-              </input>)).to.equal(true);
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      const time = wrapper.ref('time').get(0);
+      expect(time.type).to.equal("time");
+      expect(time.className).to.equal("form-control");
     });
 
     it('should have a Current Status select with correct options', function() {
-      const { wrapper } = setup()
-      expect(wrapper.contains(
-          <select className="form-control"
-            ref={node => {currentStatus = node }}>
-              <option value="In Progress">In Progress</option>
-              <option value="Resolved">Resolved</option>
-              <option value="Reopened">Reopened</option>
-              <option value="Closed">Closed</option>
-                </select>)).to.equal(true);
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      const currentStatus = wrapper.ref("currentStatus").get(0);
+      expect(currentStatus.className).to.equal("form-control");
       });
 
     it('should have a Details testArea with correct props', function() {
-      const { wrapper } = setup() 
-      expect(wrapper.contains(
-          <textarea className="form-control" placeholder="What have you been working on?
-            What do you plan to work on next?"
-            rows="5" ref={node => { details = node }}>
-              </textarea>)).to.equal(true);
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      const details = wrapper.ref('details').get(0);
+      expect(details.rows).to.equal(5);
       });
+  });
+
+  describe('Testing default values of all inputs', function() {
+    it('should have an Author input with empty default value', function() {
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      expect(wrapper.ref('author').get(0).value).to.equal("");
+    });
+
+    it('should have a ticketNum input with empty default value', function() {
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      expect(wrapper.ref('ticketNum').get(0).value).to.equal("");
+    });
+
+    it('should have a date input with empty default value', function() {
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      expect(wrapper.ref('date').get(0).value).to.equal("");
+    });
+
+    it('should have a time input with empty default value', function() {
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      expect(wrapper.ref('date').get(0).value).to.equal("");
+    });
+
+    it('should have a Current Status input with default value "In Progress"', function() {
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      expect(wrapper.ref('currentStatus').get(0).value).to.equal("In Progress");
+    });
+
+    it('should have a Details input with empty default value', function() {
+      const { wrapper, state } = mountSetup()
+      this.state = state;
+      expect(wrapper.ref('details').get(0).value).to.equal("");
+    });
   });
 
   describe('Testing the form and its props', function() {
@@ -108,11 +166,32 @@ describe('Testing the ticket updater component', function() {
       expect(form.prop('onSubmit')).to.be.a('function');
     });
 
-    it('should call a spy passed in for the onSubmit function', function() {
-      const { props, form } = setup()
+    it('should call a spy passed in for the onSubmit function after populating form values', function() {
+      const { props, wrapper, form } = mountSetup()
+    
+      // Populating values in form so it will let me submit it
+      // wrapper.find('input').first().simulate('change', {target: { value: 'Justin' }});
+      wrapper.ref('author').get(0).value = 'Justin';
+      expect(wrapper.ref('author').get(0).value).to.equal("Justin");
+      
+      wrapper.ref('ticketNum').get(0).value = 'TS-111';
+      expect(wrapper.ref('ticketNum').get(0).value).to.equal("TS-111");
+      
+      wrapper.ref('date').get(0).value = '1/1/1111';
+      expect(wrapper.ref('date').get(0).value).to.equal("1/1/1111");
+      
+      wrapper.ref('time').get(0).value = '12:34'; 
+      expect(wrapper.ref('time').get(0).value).to.equal("12:34");
 
-      // currently failing on purpose because i dont yet know how to use the
-      // spy properly with enzyme
+      wrapper.ref('currentStatus').get(0).value = 'In Progress';
+      expect(wrapper.ref('currentStatus').get(0).value).to.equal("In Progress");
+
+      wrapper.ref('details').get(0).value = 'Working hard';
+      expect(wrapper.ref('details').get(0).value).to.equal("Working hard");
+
+      const button = form.find('button');
+
+      form.simulate('submit');
       expect(props.ticketUpdate.called).to.equal(true);
     });
   });
